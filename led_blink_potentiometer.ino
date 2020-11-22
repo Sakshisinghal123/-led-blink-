d509ef877e20dd68d694f8b219eb0d2fcff5b438
Sakshisinghal123 <66126791+Sakshisinghal123@users.noreply.github.com>
2020-11-22 18:11:00 +0530

    Add files via upload

---
+++ led_blink_potentiometer.ino
@@ -0,0 +1,34 @@
+int i,j;
+float abc;
+int readval=A5;
+void setup() {
+   Serial.begin(9600);
+   pinMode(A5,INPUT);
+   for (int i=8; i<13; i++)
+   { 
+     pinMode(i,OUTPUT);
+   }
+
+}
+
+void loop() {
+  j=analogRead(readval);
+  abc=(5.0/1023.0)*j;
+  if(abc>=0) 
+  {
+    for(int i=7; i<=abc+7; i++)
+    {
+      digitalWrite(i,HIGH);
+      delay(200);
+    }
+     for(i=8+abc; i<=12; i++)
+     {
+      digitalWrite(i,LOW);
+      delay(200);
+     }
+  }
+}
+    
+  
+
+
