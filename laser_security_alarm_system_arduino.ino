105b072893b1802c3269ce7225ab7654aa155823
Sakshisinghal123 <66126791+Sakshisinghal123@users.noreply.github.com>
2020-12-12 20:33:17 +0530

    Add files via upload

---
+++ laser_security_alarm_system_arduino.ino
@@ -0,0 +1,30 @@
+int j;
+float abc;
+int ledPinA = 11;
+int buzzerPin=9;
+int ledPin=8;
+int ldrPin=A1;
+ void setup() {
+  Serial.begin(9600);
+
+ pinMode(ledPinA,OUTPUT);
+ pinMode(ledPin,OUTPUT);
+ pinMode(buzzerPin,OUTPUT);
+ pinMode(ldrPin,INPUT);
+}
+
+void loop() {
+ digitalWrite(ledPinA ,HIGH);
+ j=analogRead(A1);
+ abc=(5.0/1023.0)*j;
+ 
+ if(abc>4){
+  
+  digitalWrite(ledPin,HIGH);
+  tone(buzzerPin,1000);
+  delay(100);
+  tone(buzzerPin,0);
+  delay(100);
+ }
+
+}
